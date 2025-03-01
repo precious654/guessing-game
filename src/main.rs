808173c8d5d1ad2bf2a77d67eb10d53a44bbbf40
precious654 <odukwechiemeka@gmail.com>
2025-03-01 11:51:51 +0100

    finished building the game

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
+
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    println!("You guessed: {}", guess);
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid input, Please input a number");
+                continue;
+            },
+        };
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Your guess is less than the secret number"),
+            Ordering::Greater => println!("Your guess is greater than the secret number"),
+            Ordering::Equal => {
+                println!("Congratulations! You guessed the correct number!");
+                break;
+            }
+        }
+    }
 }
